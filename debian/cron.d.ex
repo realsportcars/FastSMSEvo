175f7ba7219676afb01830bbc9e62f9828eb7f23
Davide <realsportcars@hotmail.com>
2011-02-22 20:06:53 +0100

    deleted:    debian/cron.d.ex 	deleted:    debian/emacsen-install.ex 	deleted:    debian/emacsen-remove.ex 	deleted:    debian/emacsen-startup.ex 	deleted:    debian/fastsms.default.ex 	deleted:    debian/fastsms.doc-base.EX 	deleted:    debian/init.d.ex 	deleted:    debian/init.d.lsb.ex 	deleted:    debian/manpage.1.ex 	deleted:    debian/manpage.sgml.ex 	deleted:    debian/manpage.xml.ex 	deleted:    debian/menu.ex 	deleted:    debian/postrm.ex 	deleted:    debian/preinst.ex 	deleted:    debian/watch.ex

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fastsms package
-#
-0 4	* * *	root	[ -x /usr/bin/fastsms_maintenance ] && /usr/bin/fastsms_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fastsms):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="fastsms" command="/usr/bin/fastsms"
